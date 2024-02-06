b364f3868a85f789a3a6a37caf95452076a33aa6
hebedebe <babyyodaismine@gmail.com>
2024-02-06 14:38:55 +1100

    basic gui stuff

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from pygame.locals import *
 from pygame import Vector2
 
+import pygame_gui as gui
+
 import random
 
 pygame.init()
@@ -10,10 +12,16 @@ FPS = 6000
 TILE_SIZE = 16
 CHUNK_SIZE = Vector2(20, 20)
 
-width, height = 160 * 2, 180 * 2
+width, height = 160 * 3, 180 * 2
 display = pygame.display.set_mode((width, height), SCALED | FULLSCREEN)
 clock = pygame.time.Clock()
-dt = 0
+delta_time = 0
+ui_manager = gui.UIManager((width, height))
+debug_box = gui.elements.UITextBox(
+    "",
+    Rect(0, 0, 200, 140),
+    ui_manager
+)
 
 
 class Particle:
@@ -28,11 +36,11 @@ class Particle:
         self.kill = False
 
     def update(self):
-        self.velocity += self.gravity * dt
-        self.velocity = self.velocity.lerp(Vector2(), self.drag * dt)
-        self.pos += self.velocity * dt
+        self.velocity += self.gravity * delta_time
+        self.velocity = self.velocity.lerp(Vector2(), self.drag * delta_time)
+        self.pos += self.velocity * delta_time
 
-        self.size += self.size_per_second * dt
+        self.size += self.size_per_second * delta_time
 
     def draw(self, world):
         pygame.draw.circle(display, self.colour, world.worldToScreenPosition(self.pos), self.size)
@@ -51,14 +59,11 @@ class ExpandingCircleParticle(Particle):
         rad_spd = 4 * 100/self.target_radius
         wid_spd = rad_spd * 7/4
 
-        self.radius = pygame.math.lerp(self.radius, self.target_radius, rad_spd * dt)
-        self.width = pygame.math.lerp(self.width, self.target_width, wid_spd * dt)
+        self.radius = pygame.math.lerp(self.radius, self.target_radius, rad_spd * delta_time)
+        self.width = pygame.math.lerp(self.width, self.target_width, wid_spd * delta_time)
 
         if self.width <= 1.5:
             self.kill = True
-
-        # self.radius += dt * rad_spd
-        # self.width -= dt * wid_spd
 
     def draw(self, world):
         pygame.draw.circle(display, self.colour, world.worldToScreenPosition(self.pos), self.radius, int(self.width))
@@ -79,6 +84,7 @@ class Chunk:
 class World:
     def __init__(self):
         self.chunks = {}
+        self.local_chunks = []
 
         self.particles = []
 
@@ -124,11 +130,11 @@ class World:
         speed = 160*6
         lerp_speed = 10
         offset_lerp_speed = 10
-        self.camera_position_offset = self.camera_position_offset.lerp(Vector2(), offset_lerp_speed * dt)
-        self.target_camera_position.y -= dt * speed * (keys[K_w] - keys[K_s])
-        self.target_camera_position.x -= dt * speed * (keys[K_a] - keys[K_d])
+        self.camera_position_offset = self.camera_position_offset.lerp(Vector2(), offset_lerp_speed * delta_time)
+        self.target_camera_position.y -= delta_time * speed * (keys[K_w] - keys[K_s])
+        self.target_camera_position.x -= delta_time * speed * (keys[K_a] - keys[K_d])
         self.real_camera_position = self.real_camera_position.lerp(self.target_camera_position,
-                                                                   pygame.math.clamp(lerp_speed * dt, 0, 1))
+                                                                   pygame.math.clamp(lerp_speed * delta_time, 0, 1))
         self.camera_position = self.real_camera_position + self.camera_position_offset
 
         for particle in self.particles:
@@ -150,20 +156,20 @@ class World:
             Vector2(width, height // 2)  # right middle
         ]
 
-        chunks = []
+        self.local_chunks = []
 
         for pos in chunk_positions:
             chunk_pos = self.getChunkAtPos(pos + self.camera_position)
             if chunk_pos in self.chunks:
                 chunk = self.chunks[chunk_pos]
-                if chunk in chunks:
+                if chunk in self.local_chunks:
                     continue
             else:
                 chunk = self.generateChunk(chunk_pos)
-            chunks.append(chunk)
+            self.local_chunks.append(chunk)
 
 
-        for chunk in chunks:
+        for chunk in self.local_chunks:
             for x, c in enumerate(chunk.tiles):
                 for y, tile in enumerate(c):
                     pos = self.worldToScreenPosition(Vector2(x, y) * TILE_SIZE + chunk.pos)
@@ -202,8 +208,14 @@ while running:
     world.update()
     world.draw()
 
+    debug_box.set_text(f"FPS: {round(clock.get_fps(), 2)} <br>delta: {delta_time} <br>local chunks: "
+                       f"{len(world.local_chunks)} <br>pos: {round(world.camera_position, 2)}")
+
+    ui_manager.update(delta_time)
+    ui_manager.draw_ui(display)
+
     pygame.display.flip()
 
-    dt = pygame.math.clamp(clock.tick(FPS) / 1000, 0, 1)
+    delta_time = pygame.math.clamp(clock.tick(FPS) / 1000, 0, 1)
 
 pygame.quit()
